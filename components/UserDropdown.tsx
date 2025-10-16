ce4ad3a67f069467a125c60fdcac44f0d6e237bc
Dhruva N <dhruvanaganand@gmail.com>
2025-10-16 22:08:32 +0530

    Implement Search Stocks

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,6 +1,5 @@
 'use client';
-import {useRouter} from "next/navigation";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -9,20 +8,21 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import NavItems from "@/components/NavItems";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
 import {LogOut} from "lucide-react";
-import {Button} from "@/components/ui/button"
-import React from 'react'
+import NavItems from "@/components/NavItems";
 import {signOut} from "@/lib/actions/auth.actions";
 
-const UserDropdown = ({user}:{user:User}) => {
+const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: StockWithWatchlistStatus[]}) => {
     const router = useRouter();
 
     const handleSignOut = async () => {
-
         await signOut();
         router.push("/sign-in");
     }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -34,25 +34,25 @@ const UserDropdown = ({user}:{user:User}) => {
                         </AvatarFallback>
                     </Avatar>
                     <div className="hidden md:flex flex-col items-start">
-                      <span className="text-base font-medium text-gray-400">
-                        {user.name}
-                      </span>
+                        <span className='text-base font-medium text-gray-400'>
+                            {user.name}
+                        </span>
                     </div>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="text-grey-400">
+            <DropdownMenuContent className="text-gray-400">
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
-                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                                 {user.name[0]}
                             </AvatarFallback>
                         </Avatar>
                         <div className="flex flex-col">
-                      <span className="text-base font-medium text-gray-400">
-                        {user.name}
-                      </span>
+                            <span className='text-base font-medium text-gray-400'>
+                                {user.name}
+                            </span>
                             <span className="text-sm text-gray-500">{user.email}</span>
                         </div>
                     </div>
@@ -64,12 +64,10 @@ const UserDropdown = ({user}:{user:User}) => {
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className="hidden sm:block bg-gray-600"/>
                 <nav className="sm:hidden">
-                    <NavItems/>
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
-
             </DropdownMenuContent>
         </DropdownMenu>
-
     )
 }
 export default UserDropdown
